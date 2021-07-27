f4425ba920cbf796299ec8f9c6545b244f847494
Mateus Santos <loiolamateus7@gmail.com>
2021-07-27 11:48:35 -0300

    Hulu 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  // mode: 'jit',
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screen: {
+        '3xl': '2000px'
+      },
+      gradientColorStops: theme => ({
+        'esmaecer': '#06202A'
+      })
+    }
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  variants: ['responsive ', 'group-hover', 'active', 'hover', 'focus'],
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
 }
