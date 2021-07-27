6bc351267426ec171e2ff1fe62dad7be18a37c1b
Mateus Santos <loiolamateus7@gmail.com>
2021-07-27 11:22:20 -0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,12 @@
 module.exports = {
-  // mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      screen: {
-        '3xl': '2000px'
-      },
-      gradientColorStops: theme => ({
-        'esmaecer': '#06202A'
-      })
-    }
+    extend: {},
   },
-  variants: ['responsive ', 'group-hover', 'active', 'hover', 'focus'],
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
